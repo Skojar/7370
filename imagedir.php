ee5d3cb993bdb9263fd3fbe2c9fa8577c8de1234
Skojar <rexrebstock@msn.com>
2017-03-18 21:02:19 -0500

    Add files via upload

---
+++ imagedir.php
@@ -0,0 +1,28 @@
+<?php
+
+class Picture 
+{
+	public $filename;
+	public $width;
+	public $height;
+	
+}
+
+//reads it's directoy and returns a json file list
+$files = array();
+
+foreach (glob("*.{jpg,gif,png}", GLOB_BRACE) as $filename) { //runs through list of image files
+	$file = new Picture;
+	$file->filename = $filename;
+	list($width, $height) = getimagesize($filename);
+	$file->width = $width;
+	$file->height = $height;
+	
+	$files[] = $file;
+}
+
+
+header('Content-type: application/json');
+echo json_encode($files);
+
+?>
