206467981b7bf8d30e2ee41f6dbb0ffac7544200
Skojar <rexrebstock@msn.com>
2017-04-27 09:56:42 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -28,8 +28,6 @@ function showit(what){ //this is called by onclick attributes, in this case I th
 	//make the cancel and show divs visible
 	$( '#cancel' ).css({"display":"block"});
 	$( '#show' ).css({"display":"block"});	
-	console.log($( '#show' ).html());
-	console.log('key');
 }
 
 //-->
