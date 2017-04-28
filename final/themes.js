ca346e0f04441352a51f373f34d111e70bad70bd
Skojar <rexrebstock@msn.com>
2017-04-28 13:38:52 -0500

    Update themes.js

--- final/themes.js
+++ final/themes.js
@@ -3,16 +3,15 @@
 
 function changestylesheet(which){ 
 	$( '#theme' ).attr({href : which}); //changes the linked stylesheet 
-	//$( 'link[rel=stylesheet]' ).attr({href : which}); //changes the linked stylesheet 	
 }	
 
 function changetheme(which){
 	//change:
 	//	page title
 	//	theme style sheet
-	//	menu bar
+	//	menu bar items
 	//	citation
-	var keep = $( '#keeptheme' ).is(':checked');
+	var keep = $( '#keeptheme' ).is(':checked'); //see if "keep this color scheme" checkbox is checked.
 	switch(which){
 		case "apocalypse_world":
 			//apocalypse world
@@ -31,17 +30,19 @@ function changetheme(which){
 		case "the_sprawl":
 			//the sprawl
 			document.title = "The Sprawl MC Helper";
+			$( '#navigation' ).load( "./the_sprawl.html #nav" ); //load navigation	
+			$( '#citation').load( "./the_sprawl.html #cite"); //load citation info			
 			if(!keep) changestylesheet("sprawl.css");
 			break;
 		case "monster_of_the_week":
-			//the sprawl
+			//monster of the week
 			document.title = "Monster of the Week Keeper Helper";
 			$( '#navigation' ).load( "./monster_week.html #nav" ); //load navigation	
 			$( '#citation').load( "./monster_week.html #cite"); //load citation info				
 			if(!keep) changestylesheet("monster.css");			
 			break;
 		case "uncharted_worlds":
-			//the sprawl
+			//uncharted worlds
 			document.title = "Uncharted Worlds GM Helper";
 			$( '#navigation' ).load( "./uncharted_worlds.html #nav" ); //load navigation	
 			$( '#citation').load( "./uncharted_worlds.html #cite"); //load citation info				
