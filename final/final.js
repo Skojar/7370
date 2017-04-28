d5f0f09b62633a64f721430fe3bd03bf3e46b81e
Skojar <rexrebstock@msn.com>
2017-04-28 16:44:49 -0500

    Update final.js

--- final/final.js
+++ final/final.js
@@ -24,7 +24,7 @@ sfHover = function() { //this is taken straight from http://www.htmldog.com/arti
 if (window.attachEvent) window.attachEvent("onload", sfHover);
 
 function showit(what){ //this is called by onclick attributes, in this case I think they're all list items.
-	$( '#show' ).load(what); //load the content specified by the onclick into the show div, then
+	$( '#showquote' ).load(what); //load the content specified by the onclick into the show div, then
 	//make the cancel and show divs visible
 	$( '#cancel' ).css({"display":"block"});
 	$( '#show' ).css({"display":"block"});	
